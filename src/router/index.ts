c7136852d78d3a7c26213c4bd1d82b09f354416d
Chara Niouraki <chara@travelstaytion.com>
2025-03-01 13:40:06 +0200

    remove boilerplate code and add main routes from project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import JokeDiscovery from "@/views/JokeDiscovery.vue";
+import JokeCollection from "@/views/JokeCollection.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +8,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: JokeDiscovery,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/collection',
+      name: 'collection',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/JokeCollection.vue'),
     },
   ],
 })
