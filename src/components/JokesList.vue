7270f1f5d67f3f863cdb118b0d2496199cc8bb24
Chara Niouraki <chara@travelstaytion.com>
2025-03-02 23:08:12 +0200

    handle joke list responsiveness

--- src/components/JokesList.vue
+++ src/components/JokesList.vue
@@ -30,7 +30,7 @@ function calculateHeartFill(jokeId: number) {
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-4" v-if="jokes.length > 0">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" v-if="jokes.length > 0">
     <div
       v-for="joke in props.jokes"
       :key="joke.id"
